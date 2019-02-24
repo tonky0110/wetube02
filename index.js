688d1b811fb179d93aa8e30b2bc0debb1e924373
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-24 22:58:15 +0900

    2.5 babel-node, nodemon설치 및 설정

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 const PORT = 4000;
@@ -6,12 +7,10 @@ const handleListening = () => {
 	console.log(`Listening on: http://localhost:${PORT}`);
 };
 
-function handleHome(req, res) {
-	res.send('Hello from home!');
-}
-function handleProfile(req, res) {
-	res.send('You are on my profile');
-}
+const handleHome = (req, res) => res.send('Hello from home!');
+
+const handleProfile = (req, res) => res.send('You are on my profile');
+
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
