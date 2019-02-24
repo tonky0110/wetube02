1e123c90545433adbeeeb2d655408e515df4de5d
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-24 22:38:00 +0900

    2.4 request method 설명

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ const handleListening = () => {
 	console.log(`Listening on: http://localhost:${PORT}`);
 };
 
+function handleHome(req, res) {
+	res.send('Hello from home!');
+}
+function handleProfile(req, res) {
+	res.send('You are on my profile');
+}
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+
 app.listen(PORT, handleListening);
