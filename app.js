0fe1c5419e1dea85fa182277061c20f2b98e8456
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-25 00:04:24 +0900

    Router(global, users, videos)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
+import routes from './routes';
 
 const app = express();
 
@@ -16,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); // 보안
 app.use(morgan('dev')); // 로깅 tiny, combined, common, dev
 
-app.use('/', globalRouter);
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import routes from '../routes';
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, (res, req) => res.send('video index'));
+videoRouter.get(routes.upload, (res, req) => res.send('video Upload'));
+videoRouter.get(routes.videoDetail, (res, req) => res.send('video detail'));
+videoRouter.get(routes.editVideo, (res, req) => res.send('edit video'));
+videoRouter.get(routes.deleteVideo, (res, req) => res.send('delete video'));
 
 export default videoRouter;
