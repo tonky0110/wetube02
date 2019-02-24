0113ecb91fc3002bf43f80125655626ff8af2910
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-24 23:42:40 +0900

    2.9 MVC패턴에 따른 Router분리

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # WeTube
 
 Cloning Youtube with Vanilla and NodeJS
+
+M - Data
+V - How does the data look
+C - function that looks for the data
--- app.js
+++ app.js
@@ -2,26 +2,22 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import helmet from 'helmet';
-import logger from 'morgan';
-import { userRouter } from './router';
+import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from home!');
-
-const handleProfile = (req, res) => res.send('You are on my profile');
 
 // Middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); // 보안
-app.use(logger('dev')); // 로깅 tiny, combined, common, dev
+app.use(morgan('dev')); // 로깅 tiny, combined, common, dev
 
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
+app.use('/', globalRouter);
 app.use('/user', userRouter);
+app.use('/video', videoRouter);
 
 export default app;
