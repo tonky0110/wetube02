3e6a155be1235418e68f3ec8d385d7bfc7493e91
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-25 22:20:18 +0900

    2.13 View Engine(pug)설정 및 pug파일 생성

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import routes from '../routes';
+import { videos, upload, videoDetail, editVideo, deleteVideo } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (res, req) => res.send('video index'));
-videoRouter.get(routes.upload, (res, req) => res.send('video Upload'));
-videoRouter.get(routes.videoDetail, (res, req) => res.send('video detail'));
-videoRouter.get(routes.editVideo, (res, req) => res.send('edit video'));
-videoRouter.get(routes.deleteVideo, (res, req) => res.send('delete video'));
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
 
 export default videoRouter;
