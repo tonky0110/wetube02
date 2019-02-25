b507fb9ae9e196b91835a6c3eb948d734ef87894
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-25 22:02:59 +0900

    2.11 MVC Pattern Router/Controller분리

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import routes from '../routes';
+import { users, userDetail, editProfile, changePassword } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send('user index'));
-userRouter.get(routes.userDetail, (req, res) => res.send('user Detail'));
-userRouter.get(routes.editProfile, (req, res) => res.send('user edit'));
-userRouter.get(routes.changePassword, (req, res) => res.send('user password'));
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
