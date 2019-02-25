de174239fd448b482f8585c70b087bf9262d0dfc
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-25 23:19:11 +0900

    2.16 Local Variable using LocalsMiddleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { localsMiddleware } from './middlewares';
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
@@ -11,12 +12,14 @@ import routes from './routes';
 const app = express();
 
 // Middleware
+app.use(helmet()); // 보안
 app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); // 보안
 app.use(morgan('dev')); // 로깅 tiny, combined, common, dev
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
