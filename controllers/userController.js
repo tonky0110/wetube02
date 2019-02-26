4751a9449de1931bc563ab7b1a4cdeb0fd866fe0
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-26 22:10:57 +0900

    2.17 send Variable in Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
-export const join = (req, res) => res.render('Join');
-export const login = (req, res) => res.render('Login');
-export const logout = (req, res) => res.render('Logout');
-export const userDetail = (req, res) => res.render('User Detail');
-export const editProfile = (req, res) => res.render('Edit Profile');
-export const changePassword = (req, res) => res.render('Change Password');
+export const join = (req, res) => res.render('Join', { pageTitle: 'Join' });
+export const login = (req, res) => res.render('Login', { pageTitle: 'Login' });
+export const logout = (req, res) => res.render('Logout', { pageTitle: 'Logout' });
+export const userDetail = (req, res) => res.render('User Detail', { pageTitle: 'User Detail' });
+export const editProfile = (req, res) => res.render('Edit Profile', { pageTitle: 'Edit Profile' });
+export const changePassword = (req, res) => res.render('Change Password', { pageTitle: 'Change Password' });
