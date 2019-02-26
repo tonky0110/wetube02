eefd14fffebd3bb985deff947fde355dc4915584
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-02-26 22:41:08 +0900

    2.18 send Query Parameter on Get Method

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,14 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) => res.render('Search', { pageTitle: 'Search' });
+
+export const search = (req, res) => {
+	const { query: { term: searchingBy } = {} } = req;
+	res.render('Search', { pageTitle: 'Search', searchingBy });
+};
+
 export const upload = (req, res) => res.render('Upload', { pageTitle: 'Upload' });
+
 export const videoDetail = (req, res) => res.render('Video Detail', { pageTitle: 'Video Detail' });
+
 export const editVideo = (req, res) => res.render('Edit Video', { pageTitle: 'Edit Video' });
+
 export const deleteVideo = (req, res) => res.render('Delete Video', { pageTitle: 'Delete Video' });
