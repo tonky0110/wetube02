05a205201422f90ea88b921a90e19f80a4bde369
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-03-01 19:41:53 +0900

    3.7 Upload and creating video

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 // Middleware
 app.use(helmet()); // 보안
 app.set('view engine', 'pug');
+app.use('/uploads', express.static('uploads'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
