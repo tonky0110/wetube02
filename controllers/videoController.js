a222bd72aec8e49de240bc131c471fc2f361e605
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-03-02 01:48:41 +0900

    3.10 Delete video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -37,7 +37,7 @@ export const videoDetail = async (req, res) => {
 	try {
 		const video = await Video.findById(id);
 		console.log(video);
-		res.render('videoDetail', { pageTitle: 'Video Detail', video });
+		res.render('videoDetail', { pageTitle: video.title, video });
 	} catch (error) {
 		console.log(error);
 		res.redirect(routes.home);
@@ -55,14 +55,21 @@ export const getEditVideo = async (req, res) => {
 };
 
 export const postEditVideo = async (req, res) => {
-	const { params: { id }, body: { title, description } } = req;
-	console.log('postEditVideo: ', id, title, description);
+	const { params: { id: _id }, body: { title, description } } = req;
 	try {
-		await Video.findOneAndUpdate({ id }, { title, description });
-		res.redirect(routes.videoDetail(id));
+		await Video.findOneAndUpdate({ _id }, { title, description });
+		res.redirect(routes.videoDetail(_id));
 	} catch (error) {
 		res.redirect(routes.home);
 	}
 };
 
-export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
+export const deleteVideo = async (req, res) => {
+	const { params: { id } } = req;
+	try {
+		await Video.findOneAndRemove({ _id: id });
+	} catch (error) {
+		//
+	}
+	res.redirect(routes.home);
+};
